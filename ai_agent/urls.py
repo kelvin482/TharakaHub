3e1c12331756b953eea2f5edf6fb117d70bfd475
Your Name <your-email@example.com>
2025-11-14 10:43:55 +0300

    Removed ai_agent app

--- ai_agent/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "agent"
-
-urlpatterns = [
-    path('', views.upload_view, name='upload'),
-    path('plot/', views.plot_view, name='plot'),
-]
